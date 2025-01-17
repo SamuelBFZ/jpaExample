d3ef6c16884b177297b018775c67aba69595962d
SAMUEL BEDOYA FLOREZ <samuelbedoya309347@correo.itm.edu.co>
2025-01-17 16:51:56 -0500

    Entidad Club creada

---
+++ src/main/java/com/jpa/jpaexample/Club/Club.java
@@ -0,0 +1,20 @@
+package com.jpa.jpaexample.Club;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+@Entity
+public class Club {
+    @Id
+    @GeneratedValue (strategy = GenerationType.IDENTITY)
+    private Integer id;
+    private String name;
+}
