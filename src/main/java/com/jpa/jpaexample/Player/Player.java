c351c450f7d456ab4ec7ed3bf3811c04d649ba46
SAMUEL BEDOYA FLOREZ <samuelbedoya309347@correo.itm.edu.co>
2025-01-17 17:23:32 -0500

    Clase Player creada

--- src/main/java/com/jpa/jpaexample/Player/Player.java
+++ src/main/java/com/jpa/jpaexample/Player/Player.java
@@ -1,8 +1,28 @@
 package com.jpa.jpaexample.Player;
 
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
 
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+@Entity
 public class Player {
-    
+
+    //Un entrenador entrena un equipo
+    @Id
+    @GeneratedValue (strategy = GenerationType.IDENTITY)
     private Integer id;
     private String name;
+
+    @Column(name = "last_Name")
+    private String lastName;
+    private Integer age;
+    private String nationality;
 }
